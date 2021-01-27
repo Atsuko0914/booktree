5d7a0a89758127f00f3f6ed4f1b59dbfe0b86157
田中敦子 <atsuko@mbp.local>
2021-01-27 10:04:39 +0900

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield('title')</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script src="{{ asset('js/app.js') }}"></script>
+</head>
+<body>
+  <div id="app">
+  <!-- ナビゲーションバー -->
+    <nav>
+      <div class="container">
+      <!-- サイトロゴ -->
+
+      </div>
+    </nav>
+  </div>
+  
+</body>
+</html>
